838347d909b35e93a58a7452ff49f90811af65d1
JohnathaOlivDev <johnatha.oliv.dev@gmail.com>
2025-09-14 01:35:59 -0300

    Update application font to VT323

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,12 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { VT323 } from "next/font/google";
+
+const vt323 = VT323({
+  subsets: ["latin"],
+  weight: "400",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Pokedex Fabrica de Software",
@@ -13,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-BR">
-      <body>{children}</body>
+      <body className={vt323.className}>{children}</body>
     </html>
   );
 }
